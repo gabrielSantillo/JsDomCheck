613e7916bd1d67073e421345a777d0461827613d
GabrielSantillo <81308836+gabrielSantillo@users.noreply.github.com>
2022-07-14 18:55:02 -0600

    getting by and class and id now

--- js/app.js
+++ js/app.js
@@ -5,3 +5,11 @@ let new_p_tag = document.querySelectorAll(`.p-tag`);
 for(let counter = 0; counter < new_p_tag.length; counter++) {
     new_p_tag[counter][`innerText`] = `grabbed by my class`;
 }
+
+let another_h1 = document.getElementById(`another-h1`);
+another_h1[`innerText`] = `Another teeeext`;
+
+let another_p_tag = document.getElementsByClassName(`new-p-tag`);
+for(let counter = 0; counter < another_p_tag.length; counter++) {
+    another_p_tag[counter][`innerText`] = `grabbed by my class, diferently`
+}
